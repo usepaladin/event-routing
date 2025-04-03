7d022a3c06a5eeb0a23a9a169a136179c697784d
Jared Tucker <jtuc0006@student.monash.edu>
2025-04-03 20:13:20 +1100

    Add Dispatch creation factory

--- src/main/kotlin/paladin/router/models/dispatch/MQTTDispatcher.kt
+++ src/main/kotlin/paladin/router/models/dispatch/MQTTDispatcher.kt
@@ -1,4 +1,29 @@
 package paladin.router.models.dispatch
 
-class MQTTDispatcher {
+import paladin.router.models.configuration.brokers.MessageBroker
+import paladin.router.pojo.configuration.brokers.BrokerConfig
+import paladin.router.pojo.configuration.brokers.EncryptedBrokerAuthConfig
+import paladin.router.pojo.configuration.brokers.MQTTBrokerConfig
+import paladin.router.pojo.dispatch.MessageDispatcher
+
+data class MQTTDispatcher(
+    override val broker: MessageBroker,
+    override val config: MQTTBrokerConfig,
+    override val authConfig: EncryptedBrokerAuthConfig,
+):MessageDispatcher(){
+
+    //todo => Research and create MQTT Producer
+    private var producer: Any? = null
+
+    override fun dispatch(payload: Any) {
+        TODO("Not yet implemented")
+    }
+
+    override fun build() {
+        TODO("Not yet implemented")
+    }
+
+    override fun validate() {
+        TODO("Not yet implemented")
+    }
 }
--- src/main/kotlin/paladin/router/util/factory/MessageDispatcherFactory.kt
+++ src/main/kotlin/paladin/router/util/factory/MessageDispatcherFactory.kt
@@ -1,30 +1,99 @@
 package paladin.router.util.factory
 
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.shaded.com.google.protobuf.DynamicMessage
+import paladin.router.enums.configuration.Broker
 import paladin.router.models.configuration.brokers.MessageBroker
+import paladin.router.models.dispatch.*
 import paladin.router.pojo.configuration.brokers.*
 import paladin.router.pojo.dispatch.MessageDispatcher
 
 object MessageDispatcherFactory {
 
-    fun fromBrokerConfig(broker: MessageBroker, config: BrokerConfig): MessageDispatcher{
+    fun fromBrokerConfig(broker: MessageBroker, authConfig: EncryptedBrokerAuthConfig, config: BrokerConfig): MessageDispatcher{
         when(config){
             is SQSBrokerConfig -> {
-                TODO()
+                return SQSDispatcher(
+                    broker = broker,
+                    config = config,
+                    authConfig = authConfig
+                )
             }
             is KafkaBrokerConfig -> {
-                TODO()
+                return getSpecificKafkaDispatcher(broker, authConfig, config)
             }
             is RabbitBrokerConfig -> {
-                TODO()
+                return RabbitDispatcher(
+                    broker = broker,
+                    config = config,
+                    authConfig = authConfig
+                )
             }
             is MQTTBrokerConfig -> {
-                TODO()
+                return MQTTDispatcher(
+                    broker = broker,
+                    config = config,
+                    authConfig = authConfig
+                )
             }
             is PulsarBrokerConfig -> {
-                TODO()
+                return getSpecificPulsarDispatcher(broker, authConfig, config)
             }
             else -> {
                 throw IllegalArgumentException("Unsupported broker config type")
+            }
+        }
+    }
+
+    private fun getSpecificKafkaDispatcher(broker: MessageBroker, authConfig: EncryptedBrokerAuthConfig, config: KafkaBrokerConfig ): KafkaDispatcher<*,*>{
+        return when(broker.brokerFormat){
+            Broker.BrokerFormat.AVRO -> {
+                KafkaDispatcher<String, GenericRecord>(
+                    broker = broker,
+                    config = config,
+                    authConfig = authConfig
+                )
+            }
+            Broker.BrokerFormat.JSON -> {
+                KafkaDispatcher<String, String>(
+                    broker = broker,
+                    config = config,
+                    authConfig = authConfig
+                )
+            }
+            Broker.BrokerFormat.PROTOBUF -> {
+                KafkaDispatcher<String, DynamicMessage>(
+                    broker = broker,
+                    config = config,
+                    authConfig = authConfig
+                )
+            }
+        }
+    }
+
+    private fun getSpecificPulsarDispatcher(broker: MessageBroker, authConfig: EncryptedBrokerAuthConfig, config: PulsarBrokerConfig): PulsarDispatcher<*>{
+        return when(broker.brokerFormat){
+            Broker.BrokerFormat.AVRO -> {
+                PulsarDispatcher<GenericRecord>(
+                    broker = broker,
+                    config = config,
+                    authConfig = authConfig
+                )
+            }
+            Broker.BrokerFormat.JSON -> {
+                PulsarDispatcher<String>(
+                    broker = broker,
+                    config = config,
+                    authConfig = authConfig
+                )
+            }
+            Broker.BrokerFormat.PROTOBUF -> {
+                PulsarDispatcher<DynamicMessage>(
+                    broker = broker,
+                    config = config,
+                    authConfig = authConfig
+                )
             }
         }
     }
